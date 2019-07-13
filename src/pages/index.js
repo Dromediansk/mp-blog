76d8a7ede62d47f8ee85d053f4d6eeecd5d9f592
Pillár <Miroslav.Pillar@senacor.com>
2019-07-13 17:20:13 +0200

    init commit

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>Miroslav's thoughts</h1>
+        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
+            <p>{node.excerpt}</p>
+          </div>
+        ))
+        }
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query {
+  allMarkdownRemark {
+    edges {
+      node {
+        id
+        excerpt
+        frontmatter {
+          title
+          date
+          description
+        }
+      }
+    }
+    totalCount
+  }
+}
+`
