fbaa3811c536101cb749e34ca260ccf27e3e801f
Pillár <Miroslav.Pillar@senacor.com>
2019-07-20 12:02:53 +0200

    adding social media and discussion panel

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { graphql } from "gatsby"
 import Layout from '../components/layout'
 import styled from 'styled-components'
+import { DiscussionEmbed } from 'disqus-react'
 
 import AvatarPhoto from '../images/avatar.jpg'
+import { FacebookSquare } from 'styled-icons/boxicons-logos/FacebookSquare'
+import { LinkedinSquare } from 'styled-icons/boxicons-logos/LinkedinSquare'
 
 const Container = styled.div`
     p {
@@ -54,8 +57,42 @@ const ContactLinks = styled.ul`
     }
 `
 
-export default ({ data }) => {
+const SocialWrapper = styled.div`
+    background: #eee;
+    padding: 1rem 0;
+    width: 50%;
+    margin: 1rem auto;
+    text-align: center;
+    border-radius: 10px;
+    ul {
+        list-style-type: none;
+        display: inline-flex;
+        margin: 0;
+    }
+    svg {
+        width: 50px;
+        height: auto;
+        margin: 0 1rem;
+    }
+`
+
+const Fb = styled(FacebookSquare)`
+    fill: #3C5A99;
+`
+const Linkedin = styled(LinkedinSquare)`
+    fill: #0077B5;
+`
+
+
+export default ({ data, pageContext }) => {
     const post = data.markdownRemark;
+    const baseUrl = 'https://blog.miroslavpillar.com/';
+    const disqusShortname = 'blog-miroslavpillar-com'
+    const disqusConfig = {
+        identifier: data.markdownRemark.id,
+        title: post.title,
+        url: baseUrl + pageContext.slug
+    }
     return (
         <Layout>
             <Container>
@@ -77,6 +114,18 @@ export default ({ data }) => {
                     <li>LinkedIn: <a href="https://www.linkedin.com/in/miroslavpillar/" target="_blank" rel="noopener noreferrer">Miroslav Pillar</a></li>
                     <li>GitHub: <a href="https://github.com/Dromediansk" target="_blank" rel="noopener noreferrer">Dromediansk</a></li>
                 </ContactLinks>
+                <SocialWrapper>
+                    <h3>Share this post</h3>
+                    <ul>
+                        <li>
+                            <a href={"https://www.facebook.com/sharer/sharer.php?u=" + baseUrl + pageContext.slug} target="_blank" rel="noopener noreferrer"><Fb /></a>
+                        </li>
+                        <li>
+                            <a href={"https://www.linkedin.com/shareArticle?url=" + baseUrl + pageContext.slug} target="_blank" rel="noopener noreferrer"><Linkedin /></a>
+                        </li>
+                    </ul>
+                </SocialWrapper>
+                <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} showMedia={false} />
             </Container>
         </Layout>
     )
