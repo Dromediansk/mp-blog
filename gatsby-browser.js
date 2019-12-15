a6be4a0891c6145c178a648ed5f96e066e8c7e60
Pillár <Miroslav.Pillar@senacor.com>
2019-12-15 15:19:01 +0100

    update dialog

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onServiceWorkerUpdateReady = () => {
+    if (
+        window.confirm(
+            "This site has been updated with new data. Do you wish to reload the site to get the new data?"
+        )
+    ) {
+        window.location.reload(true);
+    }
+};
