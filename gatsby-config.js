dad79062b339c74b106af529e09fb8c52357fc4d
Pillár <Miroslav.Pillar@senacor.com>
2019-12-17 09:11:39 +0100

    rss feed config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,6 @@ module.exports = {
         article: true,
     },
     plugins: [
-        `gatsby-plugin-feed`,
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
@@ -71,6 +70,64 @@ module.exports = {
                 theme_color: `#2c5364`,
                 display: `standalone`,
                 icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.edges.map(edge => {
+                                return Object.assign({}, edge.node.frontmatter, {
+                                    description: edge.node.excerpt,
+                                    date: edge.node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    custom_elements: [{ "content:encoded": edge.node.html }],
+                                })
+                            })
+                        },
+                        query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                        description
+                        author
+                        imageBanner {
+                            publicURL
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+                        output: "/rss.xml",
+                        title: "MP Blog Feed",
+                    },
+                ],
             },
         },
         `gatsby-plugin-offline`,
