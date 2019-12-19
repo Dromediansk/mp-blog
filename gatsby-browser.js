c12bfeaf8e5575e8a5050829d4d3932d19a7a1fc
Pillár <Miroslav.Pillar@senacor.com>
2019-12-19 13:52:05 +0100

    notification test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,28 @@
-exports.onServiceWorkerUpdateReady = () => {
-    if (
-        window.confirm(
-            "MP blog has been updated. Please refresh the page to get the newest version."
-        )
-    ) {
-        window.location.reload(true);
+export const onServiceWorkerUpdateReady = () => {
+    const showNotification = () => {
+        Notification.requestPermission(result => {
+            if (result === 'granted') {
+                navigator.serviceWorker.ready.then(registration => {
+                    registration.showNotification('Update', {
+                        body: 'New content is available!',
+                        icon: 'link-to-your-icon',
+                        vibrate: [200, 100, 200, 100, 200, 100, 400],
+                        tag: 'request',
+                        actions: [ // you can customize these actions as you like
+                            {
+                                action: doSomething(), // you should define this
+                                title: 'update'
+                            },
+                            {
+                                action: doSomethingElse(), // you should define this
+                                title: 'ignore'
+                            }
+                        ]
+                    })
+                })
+            }
+        })
     }
-};
+
+    showNotification()
+}
