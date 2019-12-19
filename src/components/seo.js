346a1f4916bdf3e29288dfe645d35749fa46caba
Pillár <Miroslav.Pillar@senacor.com>
2019-12-19 22:05:29 +0100

    html lang

--- src/components/seo.js
+++ src/components/seo.js
@@ -25,7 +25,7 @@ const SEO = ({ title, description, image, pathname, article }) => (
 
             return (
                 <>
-                    <Helmet title={seo.title}>
+                    <Helmet title={seo.title} htmlAttributes={{ lang: 'en' }}>
                         <meta name="description" content={seo.description} />
                         <meta name="image" content={seo.image} />
                         {seo.url && <meta property="og:url" content={seo.url} />}
