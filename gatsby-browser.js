83e09a8db46cdf428b7777ad88f5114cf5ca4cc2
Pillár <Miroslav.Pillar@senacor.com>
2019-12-19 19:10:45 +0100

    notification update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,22 @@
+import { Refresh } from 'styled-icons/material/Refresh'
+
 export const onServiceWorkerUpdateReady = () => {
     const showNotification = () => {
         Notification.requestPermission(result => {
+            const reload = () => {
+                window.location.reload(true);
+            }
             if (result === 'granted') {
                 navigator.serviceWorker.ready.then(registration => {
                     registration.showNotification('Update', {
-                        body: 'New content is available!',
-                        icon: 'link-to-your-icon',
+                        body: 'New version is available!',
+                        icon: Refresh,
                         vibrate: [200, 100, 200, 100, 200, 100, 400],
                         tag: 'request',
-                        actions: [ // you can customize these actions as you like
+                        actions: [
                             {
-                                action: doSomething(), // you should define this
-                                title: 'update'
-                            },
-                            {
-                                action: doSomethingElse(), // you should define this
-                                title: 'ignore'
+                                action: reload(),
+                                title: 'Update'
                             }
                         ]
                     })
