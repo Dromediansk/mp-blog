11c03893ba8c279741d8dff05488792132cb0f84
Pillár <Miroslav.Pillar@senacor.com>
2019-12-19 22:51:13 +0100

    google ads

---
+++ src/components/googleAds.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+
+const GoogleAds = ({ slot }) => {
+
+    useEffect(() => {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }, [])
+
+    return (
+        <ins className='adsbygoogle'
+            style={{ display: 'block' }}
+            data-ad-client="ca-pub-4635184049553237"
+            data-ad-slot={slot}
+            data-ad-format='auto'
+            data-full-width-responsive="true"
+        >
+        </ins>
+    )
+}
+
+export default GoogleAds;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import SubscribeForm from "../components/subscribeForm"
+import GoogleAds from "../components/googleAds"
 
 const BlogWrapper = styled.div`
   border: 2px solid #ddd;
@@ -90,6 +91,7 @@ export default ({ data }) => {
   return (
     <Layout>
       <SEO />
+      <GoogleAds slot="2434444" />
       <div>
         <h1>BLOG</h1>
         <SubscribeForm />
